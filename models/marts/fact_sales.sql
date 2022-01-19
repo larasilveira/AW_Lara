18efa477668e49c46f24a134966f6d2e0985723c
laraschsilveira@gmail.com <laraschsilveira@gmail.com>
2022-01-19 23:34:08 +0000

    add_state

--- models/marts/fact_sales.sql
+++ models/marts/fact_sales.sql
@@ -14,6 +14,15 @@ order_details as (
     from {{ ref('stg_order_detail') }}
 ),
 
+orderaddress as (
+    select
+        addressid,
+        countryregion,
+        stateprovince,
+        city
+    from {{ ref('dim_address') }}
+),
+
 orders_joined as (
     select
         o.salesorderid,
@@ -24,12 +33,16 @@ orders_joined as (
         o.orderdate,
         o.subtotal,
         o.freight,
+        oa.countryregion,
+        oa.stateprovince,
+        oa.city,
         od.productid,
         od.orderqty,
         od.unitprice,
         sum(od.linetotal - (od.unitpricediscount*orderqty)) as liquid_value
     from orders o
     left join order_details od on o.salesorderid = od.salesorderid
+    left join orderaddress oa on o.shiptoaddressid = oa.addressid
     group by 
         o.salesorderid,
         o.customerid,
@@ -39,6 +52,9 @@ orders_joined as (
         o.orderdate,
         o.subtotal,
         o.freight,
+        oa.countryregion,
+        oa.stateprovince,
+        oa.city,
         od.productid,
         od.orderqty,
         od.unitprice
